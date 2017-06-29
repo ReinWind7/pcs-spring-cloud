aae417ca89e65c73ec989d9dc21bc6ab72a5affd
reinwind <reinwind7@126.com>
2017-06-29 14:18:41 +0800

    add files docker

---
+++ docker/cps_comment/src/main/java/com/ldeng/backend/CommentApplication.java
@@ -0,0 +1,28 @@
+package com.ldeng.backend;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import com.ldeng.backend.config.JwtFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CommentApplication {
+	
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		return registrationBean;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentApplication.class, args);
+	}
+}
---
+++ docker/cps_user/src/main/java/com/ldeng/backend/controller/UserController.java
@@ -0,0 +1,83 @@
+package com.ldeng.backend.controller;
+
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ldeng.backend.model.User;
+import com.ldeng.backend.service.UserService;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@RestController
+@RequestMapping ("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private EurekaClient eurekaClient;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@GetMapping("/eureka-instance")
+	public String serviceUrl() {
+	  InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("CPS_USER", false);
+	  return instance.getHomePageUrl();
+	}
+	
+	@GetMapping("/instance-info")
+	public ServiceInstance showInfo() {
+		ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+	  return localServiceInstance;
+	}
+	
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public User registerUser(@RequestBody User user) {
+		return userService.save(user);
+	}
+	
+	@RequestMapping(value="login", method=RequestMethod.POST)
+	public String login(@RequestBody Map<String, String> json) throws ServletException {
+		
+		if (json.get("username")==null || json.get("password") == null) {
+			throw new ServletException ("Please fill in username and password");
+		}
+		
+		String userName =json.get("username");
+		String password = json.get("password");
+		
+		User user = userService.findByUserName(userName);
+		
+		if (user == null) {
+			throw new ServletException ("User name not found.");
+		}
+		
+		String pwd = user.getPassword();
+		
+		if (!password.equals(pwd)) {
+			throw new ServletException("Invalid login. Please check your name and password.");
+		}
+		
+		return Jwts.builder().setSubject(userName).claim("roles", "user").setIssuedAt(new Date()).signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+				
+	}
+	
+	
+}
